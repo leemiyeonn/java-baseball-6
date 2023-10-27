6131bebfc2c4ba7e6303a89a90f14d1553c80b92
leemiyeon <lemingul7@gmail.com>
2023-10-28 01:16:37 +0900

    refactor: 사용하지 않는 상수 제거 1. BALL_AND_STRIKE 제거

--- src/main/java/baseball/enums/ResultType.java
+++ src/main/java/baseball/enums/ResultType.java
@@ -3,8 +3,7 @@ package baseball.enums;
 public enum ResultType {
     NOTHING("낫싱"),
     BALL("%d볼"),
-    STRIKE("%d스트라이크"),
-    BALL_AND_STRIKE("%d볼 %스트라이크");
+    STRIKE("%d스트라이크");
 
     private final String value;
 
